b2fe30b2551b3f33d9bc59c5496a6acc4f838f30
Haka1004 <haykuhiharutyunyan5@gmail.com>
2024-09-03 20:28:51 +0400

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,297 @@
+// import AirDatepicker from "air-datepicker";
+// import "air-datepicker/air-datepicker.css"
+// new AirDatepicker('.element')
+
+"use strict";
+
+import "./language/index.js";
+
+const url = "http://localhost:1337";
+
+// async function fetchProducts(url) {
+// 	return url
+
+// }
+
+const loading = document.querySelector('.loading')
+
+fetch(url + "/api/hero?populate=*")
+	.then((res) => res.json())
+	.then((data) => {
+		console.log(data)
+	})
+	.catch((error) => console.warn(error))
+	.finally(() => {
+		loading.remove()
+	})
+
+
+
+
+
+
+
+
+// fetch(${ url } / api / hero ? populate =*)
+// 	.then((response) => {
+// 		return response.json()
+// 	})
+// 	.then(({ data }) => {
+// 		const heroName = document.querySelector('.hero__name');
+// 		heroName.innerHTML = data.name;
+// 		const heroLastName = document.querySelector('.hero__lastName');
+// 		heroLastName.innerHTML = data.lastname;
+// 		const heroDesc = document.querySelector('.hero__descr');
+// 		heroDesc.innerHTML = data.description;
+// 		const img_desktop = document.querySelector('.hero__picture source');
+// 		const img_mobile = document.querySelector('.hero__img');
+// 		img_desktop.srcset = url + data.img_desktop.url;
+// 		img_mobile.src = url + data.img_mobile.url;
+
+// 	})
+
+// console.log(fetch("http://localhost:1337/api/hero?populate=*"));
+
+const nav = document.querySelector(".nav");
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click", () => {
+	burger.classList.toggle("burger_active");
+	nav.classList.toggle("nav_active");
+});
+
+function slider(elem) {
+	const slider = document.querySelector(`[data-slider="${elem}"]`);
+	const sliderList = slider.querySelector(".slider__list");
+	const sliderSlides = slider.querySelectorAll(".slider__slide");
+	const sliderButtonPrev = slider.querySelector(".slider__button_prev");
+	const sliderButtonNext = slider.querySelector(".slider__button_next");
+
+	sliderSlides.forEach((slide) => {
+		sliderButtonPrev.addEventListener("click", () => {
+			sliderList.scrollBy({
+				left: -slide.offsetWidth,
+				behavior: "smooth",
+			});
+			if (sliderList.scrollLeft === 0) {
+				sliderList.scrollBy({
+					left: sliderList.scrollWidth - sliderList.offsetWidth,
+				});
+			}
+		});
+
+		sliderButtonNext.addEventListener("click", () => {
+			sliderList.scrollBy({
+				left: slide.offsetWidth,
+				behavior: "smooth",
+			});
+
+			console.log(Math.floor(sliderList.scrollLeft));
+			console.log(sliderList.scrollWidth - sliderList.offsetWidth);
+
+			if (
+				Math.round(sliderList.scrollLeft) ===
+				sliderList.scrollWidth - sliderList.offsetWidth
+			) {
+				sliderList.scrollLeft = 0;
+			}
+		});
+	});
+
+	return slider;
+}
+
+slider("portfolio");
+
+//* 1st version
+
+// function popUp() {
+// 	button.addEventListener("click", () => {
+// 		popup.classList.add("popup_open");
+// document.body.style.paddingRight =
+// 	window.innerWidth - document.body.clientWidth + "px";
+// popup.style.paddingRight =
+// 	window.innerWidth - document.body.clientWidth + "px";
+// document.body.style.overflow = "hidden";
+// Array.from(document.body.children).forEach((elem) => {
+// 	elem.setAttribute("inert", "");
+// 	popup.removeAttribute("inert");
+// });
+// 	});
+// 	close.addEventListener("click", () => {
+// 		popup.classList.remove("popup_open");
+// 	popup.style.paddingRight = 0;
+// 	document.body.style.overflow = "auto";
+// 	document.body.style.paddingRight = 0;
+// 	Array.from(document.body.children).forEach((elem) => {
+// 		elem.removeAttribute("inert");
+// 		popup.setAttribute("inert", "");
+// 	});
+// });
+// 	window.addEventListener("click", (event) => {
+// 		if (event.target == popup) {
+// 			popup.classList.remove("popup_open");
+// 			popup.style.paddingRight = 0;
+// 			document.body.style.overflow = "auto";
+// 			document.body.style.paddingRight = 0;
+// 			Array.from(document.body.children).forEach((elem) => {
+// 				elem.removeAttribute("inert");
+// 				popup.setAttribute("inert", "");
+// 			});
+// 		}
+// 	});
+// 	send.addEventListener("click", () => {
+// 		popup.classList.remove("popup_open");
+// 		popup.style.paddingRight = 0;
+// 		document.body.style.overflow = "auto";
+// 		document.body.style.paddingRight = 0;
+// 		Array.from(document.body.children).forEach((elem) => {
+// 			elem.removeAttribute("inert");
+// 			popup.setAttribute("inert", "");
+// 		});
+// 	});
+// }
+
+// popUp(".popup");
+
+//* 2nd version
+
+// const popup = document.querySelector(".popup");
+// const button = document.querySelector(".footer__btn");
+// const close = document.querySelector(".popup__button");
+// const send = document.querySelector(".form__button");
+
+// function closeOpen() {
+// 	if (!popup.classList.contains("popup_open")) {
+// 		popup.classList.add("popup_open");
+// 		document.body.style.paddingRight =
+// 			window.innerWidth - document.body.clientWidth + "px";
+// 		popup.style.paddingRight =
+// 			window.innerWidth - document.body.clientWidth + "px";
+// 		document.body.style.overflow = "hidden";
+// 		Array.from(document.body.children).forEach((elem) => {
+// 			elem.setAttribute("inert", "");
+// 			popup.removeAttribute("inert");
+// 		});
+// 	} else {
+// 		popup.classList.remove("popup_open");
+// 		popup.style.paddingRight = 0;
+// 		document.body.style.overflow = "auto";
+// 		document.body.style.paddingRight = 0;
+// 		Array.from(document.body.children).forEach((elem) => {
+// 			elem.removeAttribute("inert");
+// 			popup.setAttribute("inert", "");
+// 		});
+// 	}
+// }
+
+// button.addEventListener("click", () => {
+// 	closeOpen();
+// });
+// close.addEventListener("click", () => {
+// 	closeOpen();
+// });
+// send.addEventListener("click", () => {
+// 	closeOpen();
+// });
+// window.addEventListener("click", (event) => {
+// 	if (event.target == popup) {
+// 		closeOpen();
+// 	}
+// });
+
+//* 3rd version boolean
+
+// const popup = document.querySelector(".popup");
+// const button = document.querySelector(".footer__btn");
+// const close = document.querySelector(".popup__button");
+// const send = document.querySelector(".form__button");
+
+// let isOpen = false;
+
+// function openPopup() {
+// 	isOpen = true;
+// 	popup.classList.add("popup_open");
+// 	const scrollbarWidth = window.innerWidth - document.body.clientWidth + "px";
+// 	document.body.style.paddingRight = scrollbarWidth;
+// 	popup.style.paddingRight = scrollbarWidth;
+// 	document.body.style.overflow = "hidden";
+// 	Array.from(document.body.children).forEach((elem) => {
+// 		elem.setAttribute("inert", "");
+// 	});
+// 	popup.removeAttribute("inert");
+// }
+
+// function closePopup() {
+// 	isOpen = false;
+// 	popup.classList.remove("popup_open");
+// 	popup.style.paddingRight = 0;
+// 	document.body.style.overflow = "auto";
+// 	document.body.style.paddingRight = 0;
+// 	Array.from(document.body.children).forEach((elem) => {
+// 		elem.removeAttribute("inert");
+// 	});
+// 	popup.setAttribute("inert", "");
+// }
+
+// function togglePopup() {
+// 	if (isOpen) {
+// 		closePopup();
+// 	} else {
+// 		openPopup();
+// 	}
+// }
+
+// button.addEventListener("click", togglePopup);
+// close.addEventListener("click", closePopup);
+// send.addEventListener("click", closePopup);
+// window.addEventListener("click", (event) => {
+// 	if (event.target === popup) {
+// 		closePopup();
+// 	}
+// });
+
+//* 4th version boolean
+
+const popup = document.querySelector(".popup");
+const button = document.querySelector(".footer__btn");
+const close = document.querySelector(".popup__button");
+const send = document.querySelector(".form__button");
+
+let isOpen = false;
+
+function togglePopup() {
+	isOpen = !isOpen;
+
+	if (isOpen) {
+		popup.classList.add("popup_open");
+		const scrollbarWidth = window.innerWidth - document.body.clientWidth + "px";
+		document.body.style.paddingRight = scrollbarWidth;
+		popup.style.paddingRight = scrollbarWidth;
+		document.body.style.overflow = "hidden";
+		Array.from(document.body.children).forEach((elem) => {
+			elem.setAttribute("inert", "");
+		});
+		popup.removeAttribute("inert");
+	} else {
+		popup.classList.remove("popup_open");
+		popup.style.paddingRight = 0;
+		document.body.style.overflow = "auto";
+		document.body.style.paddingRight = 0;
+		Array.from(document.body.children).forEach((elem) => {
+			elem.removeAttribute("inert");
+		});
+		popup.setAttribute("inert", "");
+	}
+}
+
+button.addEventListener("click", togglePopup);
+close.addEventListener("click", togglePopup);
+send.addEventListener("click", togglePopup);
+window.addEventListener("click", (event) => {
+	if (event.target === popup) {
+		togglePopup();
+	}
+});
+
+
---
+++ src/js/language/langList.js
@@ -0,0 +1,122 @@
+export const langList = {
+	header: {
+		home: {
+			ru: "Главная",
+			en: "Home",
+		},
+		about: {
+			ru: "Обо мне",
+			en: "About me",
+		},
+		skills: {
+			ru: "Навыки",
+			en: "Skills",
+		},
+		portfolio: {
+			ru: "Портфолио",
+			en: "Portfolio",
+		},
+		contacts: {
+			ru: "Контакты",
+			en: "Contacts",
+		},
+	},
+	hero: {
+		firstName: {
+			ru: "Денис",
+			en: "Denis",
+		},
+		lastName: {
+			ru: "Новик",
+			en: "Novik",
+		},
+		text: {
+			ru: "UX | UI дизайнер, 24 лет, Минск",
+			en: "UX | UI designer, 24 years old, Minsk",
+		},
+	},
+	about: {
+		title: {
+			en: "About me",
+			ru: "Обо мне",
+		},
+		descr1: {
+			en: "Hi, I'm Denis - UX/UI designer from Minsk. I'm interested in design and everything connected with it.",
+			ru: "Привет, я Денис — UX/UI дизайнер из Минска. Я интересуюсь дизайном и всем, что с ним связано.",
+		},
+		descr2: {
+			en: "I'm studying at courses «Web and mobile design interfaces» in IT-Academy.",
+			ru: "Я учусь на курсах «Веб и мобильное дизайн интерфейсов» в IT-Academy.",
+		},
+		descr3: {
+			en: "Ready to implement excellent projects with wonderful people.",
+			ru: "Готов к реализации отличных проектов с чудесными людьми.",
+		},
+	},
+	skills: {
+		title: {
+			en: "Skills",
+			ru: "Навыки",
+		},
+		text: {
+			en: "I work in such programs as",
+			ru: "Я работаю в таких программах как",
+		},
+	},
+	portfolio: {
+		title: {
+			ru: "Портфолио",
+			en: "Portfolio",
+		},
+	},
+	contacts: {
+		title: {
+			en: "Contacts",
+			ru: "Контакты",
+		},
+		text: {
+			en: "Want to know more or just chat? You are welcome!",
+			ru: "Хочешь узнать больше или просто поговорить? Вы всегда рады!",
+		},
+		button: {
+			ru: "Свяжитесь со мной",
+			en: "Send message",
+		},
+		text2: {
+			ru: "Мои профили в социальных сетях",
+			en: "Like me on",
+		},
+		text3: {
+			ru: "LinkedIn, Instagram, Behance, Dribble",
+			en: "LinkedIn, Instagram, Behance, Dribble",
+		},
+	},
+	popup: {
+		title: {
+			ru: "Не стесняйтесь писать мне ;)",
+			en: "Feel free to text me ;)",
+		},
+		button: {
+			ru: "Отправить",
+			en: "Send",
+		},
+	},
+	placeholder: {
+		name: {
+			ru: "Имя",
+			en: "Name",
+		},
+		email: {
+			ru: "Электронная почта",
+			en: "Email",
+		},
+		phone: {
+			ru: "Телефон",
+			en: "Phone",
+		},
+		message: {
+			ru: "Сообщение",
+			en: "Message",
+		},
+	},
+};
